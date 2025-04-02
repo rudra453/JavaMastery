5f44e2c9fad6f8378e7537eed8152ba01dab24e4
rudra453 <rudrapradhan998@gmail.com>
2025-04-02 16:43:11 +0530

    Create reverse_array.java

---
+++ Basics/Arrays/reverse_array.java
@@ -0,0 +1,22 @@
+public class reversearray{
+public static void main (String[] args) {
+int[] a={10,20,30,40};
+System.out.println("before reversing");
+for(int i:a) {
+System.out.print(i+",");
+}
+int start=0;
+int end=a.length-1;
+while(start<end) {
+int temp=a[start];
+a[start]=a[end];
+a[end]=temp;
+start++;
+end--;
+}
+System.out.println();
+System.out.println("after reversing");
+for(int i:a) {
+System.out.print(i+",");
+}
+}}
